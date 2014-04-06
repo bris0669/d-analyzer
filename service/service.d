cbe2fa3c600bc197d13f5de0e63d16641cc96875
bris0669 <bris0669@scs.ubbcluj.ro>
2014-04-06 10:55:52 +0300

    Separate web site from CGI application.

--- service/service.d
+++ service/service.d
@@ -1,7 +1,23 @@
 import std.stdio;
+import std.process;
+
+class Service {
+	this() {
+	}
+
+	void Main() {
+		write("Content-type: text/html\n\n");
+		//auto dmd = execute(["ldc2", "myapp.d"]);
+		//if (dmd.status != 0) writeln("Compilation failed:\n", dmd.output);
+
+		
+    writeln("Hello, world!");
+		writeln(environment.toAA());
+	}
+}
  
 void main()
 {
-	write("Content-type: text/html\n\n");
-    writeln("Hello, world!");
+  Service service = new Service();
+	service.Main();
 }
