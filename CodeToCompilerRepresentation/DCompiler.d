81458793deba76a69a70907f7b04bd09e3a3f4d7
bris0669 <bris0669@scs.ubbcluj.ro>
2014-08-23 20:02:11 +0300

    Done wrapper over LDC.

--- CodeToCompilerRepresentation/DCompiler.d
+++ CodeToCompilerRepresentation/DCompiler.d
@@ -1,3 +1,7 @@
+import std.file;
+import Log;
+import std.process;
+import std.conv;
 
 class DCompiler {
 	string sourceCode;
@@ -7,7 +11,22 @@ class DCompiler {
 	}
 
 	string Compile() {
-		return sourceCode;
+		string sourceFile = "/tmp/code.d";
+		std.file.write(sourceFile, sourceCode);
+		
+		return GetVerboseOutput(sourceFile);
+	}
+
+	string GetVerboseOutput(string sourceFile) {
+		string result;
+		auto process = execute(["/home/cbobby/.usrlocal/bin/ldc2", 
+		  "-vv", "-O0", "-output-ll", sourceFile]);
+		if ((process.status != 0) && (process.status != 1)) {
+			LogInfo("Compilation failed. Exit status " ~ to!string(process.status) ~ ". Output:\n" ~ process.output);
+		} else {
+			result = process.output;
+		}
+		return result;
 	}
 
 }
