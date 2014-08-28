d0462b9d7d6f7cc8cb36494f8516249c25813b0a
bris0669 <bris0669@scs.ubbcluj.ro>
2014-08-28 22:42:23 +0300

    Got allocations from compiler representation.

--- DEscapeAnalysis/DWarningsGenerator.d
+++ DEscapeAnalysis/DWarningsGenerator.d
@@ -1,4 +1,15 @@
 import StaticAnalysisWarning;
+import std.regex;
+import std.stdio;
+import std.file;
+
+class TypeAllocation {
+	string TypeName;
+	string SuperTypes;
+	int LineOfFirstAllocation;
+	int AllocationCount;
+	int DeallocationCount;
+}
 
 class DWarningsGenerator {
 	string instrumentedSourceCode;
@@ -12,6 +23,36 @@ class DWarningsGenerator {
 	StaticAnalysisWarning[] GenerateWarnings() {
 		StaticAnalysisWarning[] result;
 
+		TypeAllocation[] typeAllocations = GetTypeAllocations();
+		foreach (typeAllocation; typeAllocations) {
+			StaticAnalysisWarning warning = GetWarning(typeAllocation);
+			if (warning !is null)
+				result ~= warning;
+		}
+
+		return result;
+	}
+
+	TypeAllocation[] GetTypeAllocations() {
+		TypeAllocation[] result;
+
+		//std.file.write("/tmp/regex.txt", compilerRepresentation);
+
+		auto r = regex(`DeclarationExp::toElem: static string new_.*";`);
+		foreach(c; matchAll(compilerRepresentation, r))
+			result = PutAllocation(result, c.hit);
+		//append("/tmp/regex.txt", c.hit ~ "\n");
+		
+		r = regex(`DeclarationExp::toElem: alias \(.*\) new_super.*;`);
+		foreach(c; matchAll(compilerRepresentation, r))
+			result = PutSuperTypes(result, c.hit);
+			//append("/tmp/regex.txt", c.hit ~ "\n");
+
+		return result;
+	}
+
+	StaticAnalysisWarning GetWarning(TypeAllocation typeAllocation) {
+		StaticAnalysisWarning result;
 
 		return result;
 	}
