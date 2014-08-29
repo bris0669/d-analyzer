ee2c30feee210e6dbdcd9546b61c86a0d7b75c7f
bris0669 <bris0669@scs.ubbcluj.ro>
2014-08-29 16:59:02 +0300

    Get ActionType correctly.

--- service/service.d
+++ service/service.d
@@ -6,17 +6,31 @@ import std.array;
 import LlvmirRepresentation;
 import Log;
 import std.algorithm;
+import std.regex;
+import std.conv;
+
+enum ActionType {
+	DEscapeAnalysis, CppEscapeAnalysis, CppSafeFormat, 
+	DCompilerRepresentation, CppCompilerRepresentation
+}
 
 class Service {
+	private string urlQuery;
+
 	this() {
 	}
 
 	void Main() {
-
 		write("Content-type: text/html\n\n");
+
 		try {
-			string inputSourceCode = GetInputSourceCode();
-			//writeln(inputSourceCode);
+			urlQuery = stdin.readln();
+			if (urlQuery is null) {
+				writeln("Bad URL.");
+				return;
+			}
+			ActionType actionType = GetActionType(urlQuery);
+			string inputSourceCode = GetInputSourceCode(urlQuery);
 			LlvmirRepresentation llvmirRepresentation = new LlvmirRepresentation(inputSourceCode);
 			writeln(llvmirRepresentation.GetRepresentation());
 		} catch(Exception e) {
@@ -26,22 +40,30 @@ class Service {
 		writeln(GetLogBuffer());
 	}
 
-	string GetInputSourceCode() {
-		string inputSourceCode = "";
-		string line;
-    if ((line = stdin.readln()) !is null) {
-			line = find(line, "isc=");
-			line = line[4..$];
-			inputSourceCode = decode(line);
-			dchar[dchar] translationTable = ['+' : ' '];
-			inputSourceCode = translate(inputSourceCode, translationTable);
-			inputSourceCode = replace(inputSourceCode, "%3B", ";");
-			inputSourceCode = replace(inputSourceCode, "%2C", ",");
-			inputSourceCode = replace(inputSourceCode, "%3D", "=");
-			inputSourceCode = replace(inputSourceCode, "%2F", "/");
-			inputSourceCode = replace(inputSourceCode, "%3A", ":");
+	ActionType GetActionType(string urlQuery) {
+		//action=dCompilerRepresentation&isc=import+std.stdio%3B%0A%0Avoid+main()+%7B%0A++++writeln(%22Hello%2C+world!%22)%3B%0A%7D%0A
+		ActionType result;
+		string actionTypeAsString = matchFirst(urlQuery, regex(`action=(.*)\&`))[1];
+		string firstChar = actionTypeAsString[0..1];
+		string capitalizedActionType = firstChar.toUpper() ~ actionTypeAsString[1..$];
+		result = to!ActionType(capitalizedActionType);
+		return result;
+	}
 
-		}
+	string GetInputSourceCode(string urlQuery) {
+		//action=dCompilerRepresentation&isc=import+std.stdio%3B%0A%0Avoid+main()+%7B%0A++++writeln(%22Hello%2C+world!%22)%3B%0A%7D%0A
+		string inputSourceCode = "";
+		string line = find(urlQuery, "isc=");
+		line = line[4..$];
+		inputSourceCode = decode(line);
+		dchar[dchar] translationTable = ['+' : ' '];
+		inputSourceCode = translate(inputSourceCode, translationTable);
+		inputSourceCode = replace(inputSourceCode, "%3B", ";");
+		inputSourceCode = replace(inputSourceCode, "%2C", ",");
+		inputSourceCode = replace(inputSourceCode, "%3D", "=");
+		inputSourceCode = replace(inputSourceCode, "%2F", "/");
+		inputSourceCode = replace(inputSourceCode, "%3A", ":");
+
 		return inputSourceCode;
 	}
 
